518730492e14cdb1a506213f7b0dcaec61f77195
Ravi Ranjan Yadav <rry4298@gmail.com>
2025-10-30 19:31:01 +0530

    test

--- style.js
+++ style.js
@@ -1,4 +1,6 @@
 setTimeout(() => {
     const name = prompt('Hi Welcome to my site, please enter your name');
-    document.getElementById('name').innerHTML = "Hi! " + name;
+    if (name) {
+        document.getElementById('name').innerHTML = "Hi! " + name;
+    }
 }, 3000)
